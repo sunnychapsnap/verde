de2ae9cbfaa4b28b958a806ddfc2ce185bfab00e
Jacob Hunt <jacobhuntemail@gmail.com>
2018-05-11 15:54:46 -0400

    Added photo gallery

---
+++ javascript/photoGallery.js
@@ -0,0 +1,7 @@
+(function() { 
+  Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js');
+  Galleria.run('.galleria', {
+    autoplay: 3000,
+  });
+}());
+
